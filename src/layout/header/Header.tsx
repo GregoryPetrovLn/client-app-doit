2b312858165c4413f313fa4fcacef0ab7e308ff0
Gregory Petrov <grishap96123@gmail.com>
2022-07-19 19:03:25 +0300

    added header

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return <div className={'header-wrapper'}>
+    <span className={'header-wrapper__title'}>DOIT</span>
+  </div>;
+};
+
+export default Header;
